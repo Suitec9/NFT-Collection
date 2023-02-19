3957e2dc689cd36d53372ab1f160a644646f5b7c
Your Name <you@example.com>
2023-02-20 01:12:34 +0200

    #9s
    Brian Pictuer

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5E4A957C361e2a6268d084561e38b274d6Ef4d54";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
