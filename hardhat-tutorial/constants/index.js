e0d1f733c7e5c3ab53c9ce644e3fff09cbeebd5e
SSUitec99Double <you@example.com>
2023-02-20 04:19:42 +0200

    Suitec 9 lift
    New Branch

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x5E4A957C361e2a6268d084561e38b274d6Ef4d54";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "nft-collection-d-cbkkuufnm-suitec9.vercel.app";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
